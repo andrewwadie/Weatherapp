ca3db0cffd4f8916d8d9379724ac37f297c89fab
andrewwadie <andrewwadie12@gmail.com>
2024-01-18 19:38:50 +0200

    second commit

--- js/main.js
+++ js/main.js
@@ -1,32 +1,28 @@
 let SearchInput = document.getElementById("SearchInput");
 let Content = document.getElementById("content");
-let data=[]
+let data = [];
 
 SearchInput.addEventListener("keyup", function () {
-    GetData(SearchInput.value)
-})
-
-
+  GetData(SearchInput.value);
+});
 
 async function GetData(city) {
-    let response = await fetch(
-      `http://api.weatherapi.com/v1/forecast.json?key=2dacb28dce634504a84184045241001&q=${city}&days=3&aqi=yes&alerts=yes`
-    );
-    data = (await response.json())
-    console.log(data)
-DisplayData()
-    
-    
+  let response = await fetch(
+    `https://api.weatherapi.com/v1/forecast.json?key=2dacb28dce634504a84184045241001&q=${city}&days=3&aqi=yes&alerts=yes`
+  );
+  data = await response.json();
+  console.log(data);
+  DisplayData();
 }
-GetData("Cairo")
+GetData("Cairo");
 
 function DisplayData() {
-    let div = ``
-    for (let i = 0; i < data.forecast.forecastday.length; i++) {
-      const d = new Date(data.forecast.forecastday[i].date).toDateString(
-        "weekday"
-      );
-      div += `<div class="col-lg-4 border border-color shadow-lg border-3  my-5 text-light">
+  let div = ``;
+  for (let i = 0; i < data.forecast.forecastday.length; i++) {
+    const d = new Date(data.forecast.forecastday[i].date).toDateString(
+      "weekday"
+    );
+    div += `<div class="col-lg-4 border border-color shadow-lg border-3  my-5 text-light">
                <div class="d-flex"><h3 class="m-auto">${d}</h3> </div> 
                 <h5>${data.location.name}</h5>
                 <h1>${data.forecast.forecastday[i].day.avgtemp_c}<sup>o</sup>C</h1>
@@ -35,6 +31,6 @@ function DisplayData() {
                </div>
 
             </div>`;
-    }
-    Content.innerHTML = div
+  }
+  Content.innerHTML = div;
 }
